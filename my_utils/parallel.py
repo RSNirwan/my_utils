36b01c49c1965d2f333a85d736c0cc3a221c87fa
RSNirwan <rajbir.nirwan@gmail.com>
2021-10-18 14:59:52 +0200

    added multiprocessing map as _pmap

--- my_utils/parallel.py
+++ my_utils/parallel.py
@@ -1,5 +1,6 @@
 import multiprocessing as mp
 from typing import Any, Callable, Iterable, Iterator
+from multiprocessing import Pool as _Pool
 
 from pathos.multiprocessing import ProcessingPool as Pool
 
@@ -11,6 +12,14 @@ def pmap(func: Callable, input: Iterable, processes: int = 0) -> Iterator:
     """
     n_p = mp.cpu_count() if processes <= 0 else processes
     with Pool(n_p) as p:
+        out = p.map(func, input)
+    for o in out:
+        yield o
+
+
+def _pmap(func: Callable, input: Iterable, processes: int = 0) -> Iterable:
+    n_p = mp.cpu_count() if processes <= 0 else processes
+    with _Pool(n_p) as p:
         out = p.map(func, input)
     for o in out:
         yield o
--- test/test_parallel.py
+++ test/test_parallel.py
@@ -15,3 +15,9 @@ def test_pmaf():
     f = [lambda x: x + 1, lambda x: x + 2, lambda x: x + 3]
     a = 2
     assert list(parallel.pmaf(f, a)) == [3, 4, 5]
+
+
+def test__pmap():
+    input = 10 * [1]
+    expected_out = 10 * [2]
+    assert list(parallel._pmap(f_map, input)) == expected_out
